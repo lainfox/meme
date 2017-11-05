471fcae25e260bb5997b146dc04d62c76e7794b4
lainfox <lainfox@gmail.com>
2017-11-05 16:09:18 +0900

    Set firebase sdk by react-redux-firebase

--- src/script/components/List.js
+++ src/script/components/List.js
@@ -2,17 +2,27 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Item from './Item';
 import './list.css';
+import {getFirebase } from 'react-redux-firebase'
 
-const List = ({list}) => (
-  <ul className="list">
-    {list.map(item => 
-      <Item key={item.id} {...item} />
-    )}
-  </ul>
-)
+const List = ({memes, firebase}) => {
+	if (!memes) {
+		return (
+			<div>Loading...</div>
+		)
+	}
+
+	return (
+	  <ul className="list">
+	    {memes.map(item => 
+	      <Item key={item.id} firebase={firebase} {...item} />
+	    )}
+	  </ul>
+	)
+}
 
 List.propTypes = {
-  list: PropTypes.array.isRequired
+  memes: PropTypes.array,
+  firebase: PropTypes.object
 }
 
 export default List;
--- src/script/reducers/index.js
+++ src/script/reducers/index.js
@@ -1,5 +1,6 @@
 import {routerReducer as routing} from "react-router-redux";
 import {combineReducers} from 'redux'
+import {firebaseStateReducer as firebase} from 'react-redux-firebase'
 import {list, categoryFilter} from "./list";
 import {upload} from "./upload";
 import counter from "./counter";
@@ -7,6 +8,7 @@ import counter from "./counter";
 // const rootReducer = {
 const rootReducer = combineReducers({
   routing,
+  firebase,
   list,
   categoryFilter,
   upload,
