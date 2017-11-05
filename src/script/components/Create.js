a5f832ec72b4fa5d1489472fc31c3764f5fbacc2
lainfox <lainfox@gmail.com>
2017-11-05 20:45:04 +0900

    Adsense dynamic router?

--- src/script/components/Create.js
+++ src/script/components/Create.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import Adsense from '../components/Adsense';
 import MemeEditor from './MemeEditor';
 
 const CANVAS_WIDTH = 600;
@@ -15,9 +16,12 @@ class Create extends PureComponent {
     const memeTitle = item.id.replace(/\-/g, ' ');
 
     return (
-      <div className="create-component">
-        <h1>{memeTitle}</h1>
-        <MemeEditor item={item} uploadFile={uploadFile} ratio={ratio} dispatch={dispatch} />
+      <div className="container">
+        <Adsense client="ca-pub-7679582849263204" slot="9204057594" />
+        <div className="create-component">
+          <h1>{memeTitle}</h1>
+          <MemeEditor item={item} uploadFile={uploadFile} ratio={ratio} dispatch={dispatch} />
+        </div>
       </div>
     );
   }
