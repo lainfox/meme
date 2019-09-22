89269e63fbb4eab376f8850831cc96e7ad64d198
lainfox <lainfox@gmail.com>
2019-09-22 10:55:23 +0900

    Add ads.txt

--- src/script/config/routes.js
+++ src/script/config/routes.js
@@ -1,23 +1,22 @@
-import React from "react";
-import {Route} from "react-router";
-import {Switch} from "react-router-dom";
-import List from '../containers/List'
-import About from '../containers/About'
+import React from 'react';
+import { Route } from 'react-router';
+import { Switch } from 'react-router-dom';
+import List from '../containers/List';
+import About from '../containers/About';
 import Create from '../containers/Create';
-import SaveMeme from '../containers/SaveMeme'
+import SaveMeme from '../containers/SaveMeme';
 
-import NotFound from '../containers/NotFound'
-
+import NotFound from '../containers/NotFound';
 
 export default (
   <Switch>
-    <Route exact path='/' component={List}/>
+    <Route exact path="/" component={List} />
 
-    <Route exact path='/create' component={Create}/>
-    <Route path='/create/:memeId' component={Create}/>
-    <Route path='/about-us' component={About}/>
-    <Route path='/save-meme' component={SaveMeme}/>
+    <Route exact path="/create" component={Create} />
+    <Route path="/create/:memeId" component={Create} />
+    <Route path="/about-us" component={About} />
+    <Route path="/save-meme" component={SaveMeme} />
     <Route component={NotFound} />
     {/*<Route path="*" status={404} component={NotFound}/>*/}
   </Switch>
-)
+);
